07683c15bb8b3920cb5d3f85a4012a3567a29218
UltraPlayz <35439479+UltraPlayz@users.noreply.github.com>
2018-01-14 20:21:29 -0600

    Update dankmemes.js

--- dankmemes.js
+++ dankmemes.js
@@ -1,54 +1,56 @@
-const talkedRecently = new Set();
 const Discord = require("discord.js");
 
 var bot = new Discord.Client;
 
-var prefix = "-"
+var prefix = "/"
 
 bot.on("ready", function(message) {
-  console.log("Bedwars bot is ready with: -play");
-  bot.user.setPresence({ status: 'dnd', game: { name: '-play & -help', type: 0 } });
+  console.log("DankMemes loading...");
+  bot.user.setPresence({ status: 'online', game: { name: '/help', type: 0 } });
 });
 
-
 bot.on("message", (message) => {
-  if(message.author.bot) return;
+	if(message.author.bot) return;
 
-  if(!message.content.startsWith(prefix)) return;
-  if (talkedRecently.has(message.author.id)) return;
-  var args = message.content.substring(prefix.length).split(" ");
+	if(!message.content.startsWith(prefix)) return;
+	var args = message.content.substring(prefix.length).split(" ");
 
-  switch (args[0].toLowerCase()) {
-    case "play":
-      message.channel.send('@here Does anyone wanna play bedwars with: ' + message.author + '? If so please DM ' + message.author);
-      talkedRecently.add(message.author.id);
-      setTimeout(() => {
-          talkedRecently.delete(message.author.id);
-}, 1000 * 60 * 60);
+	switch (args[0].toLowerCase()) {
+		case "verify":
+			var unverified1 = message.guild.roles.find(x => x.name === 'Unverified');
+			var memberRole = message.guild.roles.find(x => x.name === 'Member');
+
+			if(!message.member.roles.find('name', 'Unverified')) {
+				message.channel.send("You already verified silly :)");
+			} else {
+				message.reply("You are verified, you now have full access to discord!");
+				message.member.addRole(memberRole);
+				message.member.removeRole(unverified1);
+			}
 		break;
- case "suggest":
-    var embed = new Discord.RichEmbed()
-        .addField(message.member.displayName + " has sent in a suggestion:", args.slice(1, args.length).join(' '))
-        .setColor(0x3fd53f)
-        .setFooter("Like it? 👍 Or 👎")
-        .setThumbnail(message.author.avatarURL)
-    message.guild.channels.find(x => x.name === "suggestions").sendEmbed(embed);
-    message.channel.send(message.author + " Your  suggestion has been sent in. Check it out in #suggestions. This new type of suggestion is still currently in beta so there may be some bugs.")
-    break;
-  case "members":
-    message.channel.send(message.guild.memberCount)
-    break;
+		case "help":
+			var embed = new Discord.RichEmbed()
+				.setTitle("Here is the help list for `SuperDankUnicorn`")
+				.addField("General Fun Commands:", "**/meme help** Shows this help list.")
+				.addField("Error", "Failed to load up commands. More commands coming soon.")
+				.setColor(0x3fd53f)
+				.setFooter("Thanks for using SuperDankUnicorn!")
+		break;
 		default:
-			message.channel.send("**That is not a command i recognize, there is currently no help list so please contact UltraPlayz.**");
+			message.channel.send("**That is not a command i recognize please type /meme help for a list of commands.**");
 		break;
 	}
 });
 
 bot.on("guildMemberAdd", member => {
-	const channel = member.guild.channels.find("name", "lobby");
+	const joinChannel = member.guild.channels.find("name", "welcome");
+	var unverified = member.guild.roles.find(x => x.name === 'Unverified')
+	const verify = member.guild.channels.find("name", "verification");
 
-	if (!channel) return;
-	channel.send(`**YAY!** ${member} **Has just joined! :)**`);
+	if (!joinChannel) return;
+	joinChannel.send(`${member} *walks out of a spaceship*, **YAY your here!**`)
+	verify.send(`${member} **Please verfiy your account by typing  /verify**`)
+	member.addRole(unverified);
 });
 
 bot.login(process.env.BOT_TOKEN);
